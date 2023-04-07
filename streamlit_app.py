f033eadf0785da249f5a6fafaa4e2b52b9006f8e
vhenchavz <129965337+vhenchavz@users.noreply.github.com>
2023-04-07 21:04:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,19 +26,26 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response 
+
+#create the reputable code block (called a function)
+        def get_fruityvice_data(this_fruit_choice):
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+            # take the json version of the response and normalize it 
+            fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+            # output it in the screen as table
+            return fruityvice_normalized
+        
 streamlit.header("Fruityvice Fruit Advice!")
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        # take the json version of the response and normalize it 
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        # output it in the screen as table
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data (fruit_choice)
+        streamlit.dataframe(back_from_function)
+       
 except URLError as e:
-  streamlit.error()
+streamlit.error()
         
 # streamlit.text(fruityvice_response.json()) #writes the data on screen
 
