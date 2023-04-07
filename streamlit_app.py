282c8cb23c1aac26af2f92543eb19f79443f4cf5
vhenchavz <129965337+vhenchavz@users.noreply.github.com>
2023-04-07 21:13:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,15 +51,25 @@ except URLError as e:
 
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+streamlit.header("The Fruit Load List Contains:")
+#Snowflake related functions 
+def get_fruit_load_list():
+        with my_cnx.cursor() as my_cur:
+                my_cur.execute("select * from fruit_load_list")'
+                return my_cur.fetchall()
+                
+ # Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+        my_data_rows = get_fruit_load_list()
+        streamlit.dataframe(my_data_rows)
+        
+        
+
 # my_cur.execute("use warehouse PC_RIVERY_WH")
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * FROM fruit_load_list")
 # my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-streamlit.header("The Fruit Load List Contains:")
-streamlit.dataframe(my_data_rows)
+
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('The user entered ', add_my_fruit)
